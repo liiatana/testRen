ab7965fcd00ae378dc6ca81e38f4348d4990cf0e
liiatana <31036379+liiatana@users.noreply.github.com>
2021-10-09 19:52:53 +0300

    addons: logger: fix

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -3,13 +3,15 @@ package base;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 
 public class TestBase {
     protected static final TestManager tmanager = new TestManager(System.getProperty("browser", BrowserType.CHROME));
@@ -22,12 +24,12 @@ public class TestBase {
 
     @BeforeMethod
     public void logTestStart(Method m, Object[] p) {
-        logger.debug("start " + m.getName() + " with parametrs " + Arrays.asList(p));
+        logger.info("Start test: {} with parameters {}", m.getName(), asList(p));
     }
 
     @AfterMethod(alwaysRun = true)
-    public void logTestStop(Method m) {
-        logger.debug("end " + m.getName());
+    public void logTestStop(Method m, ITestResult testResult) {
+        logger.info("Test {} finished with status = {}", m.getName(), testResult.getStatus());
     }
 
     @AfterSuite(alwaysRun = true)
